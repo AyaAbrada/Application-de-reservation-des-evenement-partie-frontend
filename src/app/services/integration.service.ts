b9d12daa0b51810bc2077a1b881812390d620301
AyaAbrada <abradaaya2@gmail.com>
2025-05-29 11:02:25 +0100

    Login - Frontend

--- src/app/services/integration.service.ts
+++ src/app/services/integration.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {catchError, Observable, throwError} from 'rxjs';
 import { LoginRequest } from '../models/login-request';
 import { LoginResponse } from '../models/login-response';
-
-const API = "http://localhost:8080/api/v1/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class IntegrationService {
+  private readonly API_URL = 'http://localhost:8080/api/v1/auth';
+
   constructor(private http: HttpClient) {}
 
-  doLogin(request: LoginRequest): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>(API, request);
-  }
+
+
+doLogin(request: LoginRequest): Observable<LoginResponse> {
+    console.log(request.username +"  "+ request.password);
+  return this.http.post<LoginResponse>(`${this.API_URL}/login`, request);
+}
+
+
 }
